f54a21e86bb064f1392c9eb5610bb9864fcf3101
SilverD3 <mrboornesilver@gmail.com>
2023-06-18 18:01:44 +0100

    Operators

---
+++ operators.js
@@ -0,0 +1,47 @@
+/**
+ * operator: +
+ * Used for addition and concatenation
+ */
+
+// Addition
+let nbGirls = 20, nbBoys = 30, nbStudents = nbGirls + nbBoys;
+console.log(nbStudents);
+
+// Concatenation
+console.log("There is " + nbGirls + " girls and " + nbBoys + " boys.");
+
+/**
+ * operator: -
+ * Used for substraction
+ */
+let initialAmount = 10000, spentAmount = 4500, remainingAmount = initialAmount - spentAmount;
+console.log("Remaining amount is: " + remainingAmount);
+
+/**
+ * operator: *
+ * Used for multiplication
+ */
+let grade = 12, multiplier = 3, finalGrade = grade * multiplier;
+console.log(finalGrade);
+
+/**
+ * operator: ===
+ * Used to check if values are equals
+ */
+console.log(grade === multiplier);
+
+/**
+ * operator: !==
+ * Used to check if values aren't equals
+ */
+console.log(grade !== multiplier);
+
+/**
+ * operator: +=
+ * Additon assigment
+ */
+let a = 50;
+let b = 20;
+a += b;
+
+console.log(a);
