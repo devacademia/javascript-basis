fcba2395a7d5a6f059b7c1e84b6f9e4615716a70
SilverD3 <mrboornesilver@gmail.com>
2023-06-18 18:01:19 +0100

    Use variables

---
+++ variables.js
@@ -0,0 +1,29 @@
+/**
+ * Following is how to use variables in JS
+ */
+
+let username = "Silevester";
+const country = "Cameroon";
+
+console.log("User name: " + username);
+console.log("Country: " + country);
+
+let userAge = 20;
+console.log("The age of " + username + " is " + userAge);
+
+let isAlive = true;
+console.log("Is " + username + " alive : " + isAlive);
+
+// This variable contains user' hobbies
+let hobbies = ["Videos Games", "Coding", "Watch TV"];
+let scores = [12, 4, 13, 10, "Max"];
+console.log("The hobbies of " + username + " are " + hobbies);
+
+let user = {
+  username: "Silevester",
+  country: "Cameroun",
+  age: 20,
+  isAlive: true,
+  hobbies: ["Videos Games", "Coding", "Watch TV"]
+};
+console.log(user);
